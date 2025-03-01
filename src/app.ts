2e70a1e8b41acf2497c615b3ae4e30b9a36c3381
danielqueirozc <danielqueiroz.job@gmail.com>
2025-03-01 15:42:25 -0300

    create folders http, controllers and separate routes"

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,7 @@
 import fastify from 'fastify'
-import { z } from 'zod'
-import { prisma } from './lib/prisma'
-import { stat } from 'fs'
+import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
- app.post('/users', async (request, reply) => {
-    const registerBodySchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string().min(6),
-    })
+app.register(appRoutes)
 
-    const { name, email, password } = registerBodySchema.parse(request.body) // safe vai disparar um throw se a validação falhar e vai para o codigo
-
-    await prisma.user.create({
-        data: {
-            name,
-            email,
-            password_hash: password
-        }
-    })
-
-    return reply.status(201).send()
- }) 
